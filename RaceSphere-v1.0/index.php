2f6f078147ee690d80ec6400f970f6e758f94b02
Paulo André Leal <pauloandeleal@gmail.com>
2023-04-11 13:53:19 +0100

    all to fudging PHP bois

--- RaceSphere-v1.0/index.php
+++ RaceSphere-v1.0/index.php
@@ -5,9 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
     <title>Página Principal</title>
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/pauloLeal.css">
