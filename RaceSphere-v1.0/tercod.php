f48f2771a8feafa7f53955bd4ca340fd4ad4569c
Silvafire <128393372+Silvafire@users.noreply.github.com>
2023-04-14 11:43:31 +0100

    a

--- RaceSphere-v1.0/tercod.php
+++ RaceSphere-v1.0/tercod.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
         <?php include('bootstrapInc.php'); ?>
-        <link rel="stylesheet" href="css/main.css">
+
         <link rel="stylesheet" href="css/luissilva.css">
         <title>Termos e condições</title>
 </head>
