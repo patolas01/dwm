bd6c4e470b5a3e5f82669e094985c12042422af3
Paulo André Leal <pauloandeleal@gmail.com>
2023-04-14 12:17:30 +0100

    tebas

--- RaceSphere-v1.0/index.php
+++ RaceSphere-v1.0/index.php
@@ -13,7 +13,6 @@
     <div class="results">
 
         <div class="resultCard" id="wrc">
-            <h2 class="cardTitle">WRC</h2>
             <div class="place">
                 <div class="number">3</div>
                 <div class="driverPic" id="3"><img src="img/perez.png" alt="driver"></div>
@@ -39,9 +38,34 @@
                 </div>
             </div>
         </div>
-        <div class="resultCard" id="f1">
 
+        <div class="resultCard" id="f1">
+            <div class="place">
+                <div class="number">3</div>
+                <div class="driverPic" id="3"><img src="img/perez.png" alt="driver"></div>
+                <div class="driverName" id="3">
+                    <h4 class="firstName">Sergio</h4>
+                    <p class="lastName">Perez</p>
+                </div>
+            </div>
+            <div class="place">
+                <div class="number">1</div>
+                <div class="driverPic" id="1"><img src="img/max.png" alt="driver"></div>
+                <div class="driverName" id="1">
+                    <h4 class="firstName">Max</h4>
+                    <p class="lastName">Verstappen</p>
+                </div>
+            </div>
+            <div class="place">
+                <div class="number">2</div>
+                <div class="driverPic" id="2"><img src="img/alo.png" alt="driver"></div>
+                <div class="driverName" id="2">
+                    <h4 class="firstName">Fernando</h4>
+                    <p class="lastName">Alonso</p>
+                </div>
+            </div>
         </div>
+
         <div class="resultCard" id="motogp">
         </div>
     </div>
