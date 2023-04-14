ee7fd3eea2ae174a6ae5a46c10111dfce0556b2a
Paulo André Leal <pauloandeleal@gmail.com>
2023-04-14 11:44:51 +0100

    navbar semi working

--- RaceSphere-v1.0/navbar.php
+++ RaceSphere-v1.0/navbar.php
@@ -5,22 +5,21 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a class="navbar-brand" href="#">Hidden brand</a>
+            <a class="navbar-brand" href="index.php">Navbar Lixo</a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="f1-index.php">F1</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="rally-index.php">WRC</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link" href="motogp-index.php">MotoGP</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Showroom</a>
                 </li>
             </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
         </div>
     </div>
 </nav>
