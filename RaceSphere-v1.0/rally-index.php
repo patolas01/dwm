663d2ae47c626a8dbc0cee657de4a69cc7e70f14
Licosapo <128393331+Licosapo@users.noreply.github.com>
2023-04-14 12:03:12 +0100

    sticky side bar done

--- RaceSphere-v1.0/rally-index.php
+++ RaceSphere-v1.0/rally-index.php
@@ -1,33 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RaceSphere Rally</title>
     <?php
-        include 'bootstrapInc.php';
+    include 'bootstrapInc.php';
     ?>
     <link rel="stylesheet" href="css/danielribeiro.css">
 </head>
+
 <body>
     <?php
-        include 'navbar.php';
+    include 'navbar.php';
     ?>
-    <div id="rightinfo">
-        <h3 id="titulolateral">Vodafone Rally de Portugal 2023</h3>
-    <div id="logoEvento">
-        <img src="img/logo-vodafone-rally-de-portugal-2023.png">
-    </div>
-        <div class="timer" id="timer">
-            <div class="timer" id="days"></div>
-            <div class="timer" id="hours"></div>
-            <div class="timer" id="minutes"></div>
-            <div class="timer" id="seconds"></div>
-        </div>
+    <div class="sticky-top" id="rightinfo">
+        
+            <h3 id="titulolateral">Vodafone Rally de Portugal 2023</h3>
+            <div id="logoEvento">
+                <img src="img/logo-vodafone-rally-de-portugal-2023.png">
+            </div>
+            <div class="timer" id="timer">
+                <div class="timer" id="days"></div>
+                <div class="timer" id="hours"></div>
+                <div class="timer" id="minutes"></div>
+                <div class="timer" id="seconds"></div>
+            </div>
+        
     </div>
     <div id="principal">
         <div class=""></div>
     </div>
 </body>
+
 </html>
