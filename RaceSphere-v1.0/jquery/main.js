f7c475a5f5dc5369be06440f933e34ff58a565d2
Licosapo <128393331+Licosapo@users.noreply.github.com>
2023-04-16 15:05:52 +0100

    Patolas vê como tá

--- RaceSphere-v1.0/jquery/main.js
+++ RaceSphere-v1.0/jquery/main.js
@@ -13,16 +13,13 @@ function makeTimer() {
 			var days = Math.floor(timeLeft / 86400); 
 			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
 			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
   
 			if (hours < "10") { hours = "0" + hours; }
 			if (minutes < "10") { minutes = "0" + minutes; }
-			if (seconds < "10") { seconds = "0" + seconds; }
 
-			$("#days").html(days + "<span>Days</span>");
-			$("#hours").html(hours + "<span>Hours</span>");
-			$("#minutes").html(minutes + "<span>Minutes</span>");
-			$("#seconds").html(seconds + "<span>Seconds</span>");		
+			$("#days").html(days + "<span>Dias</span>");
+			$("#hours").html(hours + "<span>Hrs</span>");
+			$("#minutes").html(minutes + "<span>Min</span>");		
 
 	}
 
--- RaceSphere-v1.0/rally-index.php
+++ RaceSphere-v1.0/rally-index.php
@@ -17,34 +17,48 @@
     include 'navbar.php';
     ?>
     <div class="sticky-top" id="rightinfo">
-
-        <h3 id="titulolateral">Vodafone Rally de Portugal 2023</h3>
+        <div id="nextEvent">
+            <h2>Próximo Evento</h2>
+        </div>
+        <div id="rightTitles">
+            <h3 id="titulolateral">Vodafone Rally de Portugal 2023</h3>
+            <h3 id="titulolateral">11-15 Abril</h3>
+        </div>
         <div id="logoEvento">
-            <img src="img/logo-vodafone-rally-de-portugal-2023.png">
+            <img src="img/img_daniel/logo-vodafone-rally-de-portugal-2023.png">
         </div>
         <div class="timer" id="timer">
             <div class="timer" id="days"></div>
             <div class="timer" id="hours"></div>
             <div class="timer" id="minutes"></div>
-            <div class="timer" id="seconds"></div>
         </div>
 
     </div>
     <div id="principal">
-        
+
         <div class="container-xxl text-center">
             <div class="row">
                 <div class="col">
-                    <div class="caixa"></div>
+                    <div id="caixa1">
+                        <div class="descricaoNoticia">Piloto e co-piloto morrem em despiste no Rally Villa de Tineo em
+                            Espanha</div>
+                    </div>
                 </div>
                 <div class="col">
-                    <div class="caixa"></div>
+                    <div id="caixa2">
+                        <div class="descricaoNoticia">Vodafone Rally de Portugal renova excelência Ambiental FIA</div>
+                    </div>
                 </div>
                 <div class="col">
-                    <div class="caixa"></div>
+                    <div id="caixa3">
+                        <div class="descricaoNoticia">A ligação de Al-Attiyah ao Vodafone Rally de Portugal</div>
+                    </div>
                 </div>
                 <div class="col">
-                    <div class="caixa"></div>
+                    <div id="caixa4">
+                        <div class="descricaoNoticia">Kalle Rovanperä vence na Estónia e aumenta vantagem no Mundial de
+                            ralis</div>
+                    </div>
                 </div>
             </div>
         </div>
