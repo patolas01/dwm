4ab132a2a51803e5c18efd2ad87153574179ac4b
Paulo André Leal <pauloandeleal@gmail.com>
2023-04-16 12:30:32 +0100

    cards dos resultados com logo

--- RaceSphere-v1.0/index.php
+++ RaceSphere-v1.0/index.php
@@ -13,68 +13,74 @@
 
     <h1>Resultados</h1>
     <div class="results">
-        <div class="resultCard" id="wrc">
-            <div class="place">
-                <div class="number">3</div>
-                <div class="driverPic" id="3"><img src="img/perez.png" alt="driver"></div>
-                <div class="driverName" id="3">
-                    <h4 class="firstName">Sergio</h4>
-                    <p class="lastName">Perez</p>
+        <div class="outerCard">
+            <img class="outerCard-img" src="img\logo-vodafone-rally-de-portugal-2023.png" alt="">
+            <div class="resultCard" id="wrc">
+                <div class="place">
+                    <div class="number">3</div>
+                    <div class="driverPic" id="3"><img src="img/perez.png" alt="driver"></div>
+                    <div class="driverName" id="3">
+                        <h4 class="firstName">Sergio</h4>
+                        <p class="lastName">Perez</p>
+                    </div>
                 </div>
-            </div>
-            <div class="place">
-                <div class="number">1</div>
-                <div class="driverPic" id="1"><img src="img/max.png" alt="driver"></div>
-                <div class="driverName" id="1">
-                    <h4 class="firstName">Max</h4>
-                    <p class="lastName">Verstappen</p>
+                <div class="place">
+                    <div class="number">1</div>
+                    <div class="driverPic" id="1"><img src="img/max.png" alt="driver"></div>
+                    <div class="driverName" id="1">
+                        <h4 class="firstName">Max</h4>
+                        <p class="lastName">Verstappen</p>
+                    </div>
                 </div>
-            </div>
-            <div class="place">
-                <div class="number">2</div>
-                <div class="driverPic" id="2"><img src="img/alo.png" alt="driver"></div>
-                <div class="driverName" id="2">
-                    <h4 class="firstName">Fernando</h4>
-                    <p class="lastName">Alonso</p>
+                <div class="place">
+                    <div class="number">2</div>
+                    <div class="driverPic" id="2"><img src="img/alo.png" alt="driver"></div>
+                    <div class="driverName" id="2">
+                        <h4 class="firstName">Fernando</h4>
+                        <p class="lastName">Alonso</p>
+                    </div>
                 </div>
             </div>
         </div>
 
 
 
+        <div class="outerCard">
+            <img class="outerCard-img" src="img/f1-logo.svg" alt="">
+            <div class="resultCard" id="f1">
+                <div class="place">
+                    <div class="number">3</div>
+                    <div class="driverPic" id="3"><img src="img/perez.png" alt="driver"></div>
+                    <div class="driverName" id="3">
+                        <h4 class="firstName">Sergio</h4>
+                        <p class="lastName">Perez</p>
+                    </div>
+                </div>
+                <div class="place">
+                    <div class="number">1</div>
+                    <div class="driverPic" id="1"><img src="img/max.png" alt="driver"></div>
+                    <div class="driverName" id="1">
+                        <h4 class="firstName">Max</h4>
+                        <p class="lastName">Verstappen</p>
+                    </div>
+                </div>
+                <div class="place">
+                    <div class="number">2</div>
+                    <div class="driverPic" id="2"><img src="img/alo.png" alt="driver"></div>
+                    <div class="driverName" id="2">
+                        <h4 class="firstName">Fernando</h4>
+                        <p class="lastName">Alonso</p>
+                    </div>
+                </div>
 
-        <div class="resultCard" id="f1">
-            <div class="place">
-                <div class="number">3</div>
-                <div class="driverPic" id="3"><img src="img/perez.png" alt="driver"></div>
-                <div class="driverName" id="3">
-                    <h4 class="firstName">Sergio</h4>
-                    <p class="lastName">Perez</p>
-                </div>
             </div>
-            <div class="place">
-                <div class="number">1</div>
-                <div class="driverPic" id="1"><img src="img/max.png" alt="driver"></div>
-                <div class="driverName" id="1">
-                    <h4 class="firstName">Max</h4>
-                    <p class="lastName">Verstappen</p>
-                </div>
-            </div>
-            <div class="place">
-                <div class="number">2</div>
-                <div class="driverPic" id="2"><img src="img/alo.png" alt="driver"></div>
-                <div class="driverName" id="2">
-                    <h4 class="firstName">Fernando</h4>
-                    <p class="lastName">Alonso</p>
-                </div>
-            </div>
-
         </div>
 
-
-        <div class="resultCard" id="motogp">
+        <div class="outerCard">
+            <img class="outerCard-img" src="img\logo-vodafone-rally-de-portugal-2023.png" alt="">
+            <div class="resultCard" id="motogp">
+            </div>
         </div>
-
     </div>
 </body>
 
