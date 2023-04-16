e8c3c7159dd543799539d6a02554fe2d0c25b54d
Licosapo <128393331+Licosapo@users.noreply.github.com>
2023-04-16 16:04:24 +0100

    patolas ve de novo

--- RaceSphere-v1.0/rally-index.php
+++ RaceSphere-v1.0/rally-index.php
@@ -51,6 +51,48 @@
                     </div>
                 </div>
             </div>
+            <div id="schedule">
+                <div class="container text-center">
+                    <div class="row">
+                        <div class="col">
+                            12 <span>Maio</span>
+                        </div>
+                        <div class="col-6">
+                            Sprint <span>15:00</span>
+                        </div>
+                        <div class="col">
+                            <i id="seta" class="fa fa-arrow-right" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="schedule">
+                <div class="container text-center">
+                    <div class="row">
+                        <div class="col">
+                            13 <span>Maio</span>
+                        </div>
+                        <div class="col-6">
+                            Race <span>17:00</span>
+                        </div>
+                        <div class="col">
+                            <i id="seta" class="fa fa-arrow-right" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="schedule">
+                <div class="container text-center">
+                    <div class="row">
+                        <div class="col-9">
+                            Mais horários
+                        </div>
+                        <div class="col">
+                            <i id="seta" class="fa fa-arrow-right" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <div id="principal">
