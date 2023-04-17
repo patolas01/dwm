d53e1d2b09030f7c7b38bf610b68a3fd84ee3eb4
pmgraca._.2409 <72268644+p4-hack3r@users.noreply.github.com>
2023-04-17 14:54:13 +0100

    conimbrinbs

---
+++ RaceSphere-v1.0/wec-index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <?php include('bootstrapInc.php'); ?>
+    <title>FIA WEC</title>
+    <link rel="stylesheet" href="css/paulograca.css">
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <div id="left">
+        <div id="newsTitle">
+            <hr class="hrTitle">
+            <h1> Noticias </h1>
+            <hr class="hrTitle">
+        </div>
+    </div>
+    <div class="vl"></div>
+    <div id="right">
+        <div id="newsTitle">
+            <hr class="hrTitle">
+            <h1> Resultados </h1>
+            <hr class="hrTitle">
+        </div>
+    </div>
+</body>
+
+</html>
