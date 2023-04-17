0c245161846ebabb4b71856391ba9e563901d91d
Silvafire <128393372+Silvafire@users.noreply.github.com>
2023-04-17 21:49:45 +0100

    a

--- RaceSphere-v1.0/footer.php
+++ RaceSphere-v1.0/footer.php
@@ -1,10 +1,12 @@
-<footer class="my-5 pt-5 text-muted text-center text-small">
-        <p class="mb-1">© RaceSphere</p>
-        <ul class="list-inline">
-            <li class="list-inline-item"><a href="sobre.php">Sobre</a></li>
-            <li class="list-inline-item"><a href="tercod.php">Termos e Condições</a></li>
-            <li class="list-inline-item"><a href="privacyPolicy.php">Política e Privacidade</a></li>
-            <li class="list-inline-item"><a href="FAQ.php">FAQ</a></li>
-            <li class="list-inline-item"><a href="contactos.php">Contactos</a></li>
-        </ul>
-</footer>
+<div class="container">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="sobre.php" class="nav-link px-2 text-muted">Sobre</a></li>
+      <li class="nav-item"><a href="tercod.php" class="nav-link px-2 text-muted">Termos e Condições</a></li>
+      <li class="nav-item"><a href="privacyPolicy.php" class="nav-link px-2 text-muted">Política e Privacidade</a></li>
+      <li class="nav-item"><a href="FAQ.php" class="nav-link px-2 text-muted">FAQs</a></li>
+      <li class="nav-item"><a href="contactos.php" class="nav-link px-2 text-muted">Contactos</a></li>
+    </ul>
+    <p class="text-center text-muted">© 2023 RaceSphere</p>
+  </footer>
+</div>
