484050e38fedeb8a765235fad21d83263496f72a
Silvafire <128393372+Silvafire@users.noreply.github.com>
2023-04-17 21:52:14 +0100

    a

--- RaceSphere-v1.0/footer.php
+++ RaceSphere-v1.0/footer.php
@@ -4,7 +4,7 @@
       <li class="nav-item"><a href="sobre.php" class="nav-link px-2 text-muted">Sobre</a></li>
       <li class="nav-item"><a href="tercod.php" class="nav-link px-2 text-muted">Termos e Condições</a></li>
       <li class="nav-item"><a href="privacyPolicy.php" class="nav-link px-2 text-muted">Política e Privacidade</a></li>
-      <li class="nav-item"><a href="FAQ.php" class="nav-link px-2 text-muted">FAQs</a></li>
+      <li class="nav-item"><a href="FAQ.php" class="nav-link px-2 text-muted">FAQ's</a></li>
       <li class="nav-item"><a href="contactos.php" class="nav-link px-2 text-muted">Contactos</a></li>
     </ul>
     <p class="text-center text-muted">© 2023 RaceSphere</p>
