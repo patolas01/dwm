77a67740a9132e2245cdb18b8fdab5df8e774006
Paulo André Leal <pauloandeleal@gmail.com>
2023-04-19 01:43:52 +0100

    doneniggas

--- RaceSphere-v1.0/navbar.php
+++ RaceSphere-v1.0/navbar.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
             aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-brand"><img src="img/logo.svg" alt="RaceSphere" height="30" width="30"></span>
+            <span class="navbar-brand"><img src="img/logo.svg" alt="RaceSphere" height="60" width="60"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
             <a class="navbar-brand " href="index.php"><img src="img/logoExt.svg" alt="RaceSphere" height="30"
@@ -17,7 +17,6 @@
                 <li class="nav-item">
                     <a class="nav-link" href="wec-index.php">WEC</a>
                 </li>
-
                 <li class="nav-item">
                     <a class="nav-link" href="showroom.php">Showroom</a>
                 </li>
