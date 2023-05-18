56c4c5186137582240d45bee5ebfb7faf7a22ae7
Paulo André Leal <pauloandeleal@gmail.com>
2023-05-18 16:32:43 +0100

    asdasd

--- RaceSphere-v1.0/admin/navbar.php
+++ RaceSphere-v1.0/admin/navbar.php
@@ -2,23 +2,24 @@
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
             aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-brand"><img src="img/logo.svg" alt="RaceSphere" height="60" width="60"></span>
+            <span class="navbar-brand"><img src="../img/logo.svg" alt="RaceSphere" height="60" width="60"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a class="navbar-brand " href="index.php"><img src="img/logoExt.svg" alt="RaceSphere" height="30"
+            <a class="navbar-brand " href="index.php"><img src="../img/logoExt.svg" alt="RaceSphere" height="30"
                     class="d-inline-block align-top"></a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="f1-index.php">Utilizadores</a>
+                    <a class="nav-link" href="users.php">Utilizadores</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="rally-index.php">Noticias</a>
+                    <a class="nav-link" href="news.php">Noticias</a>
+                </li>
+                <!--Dropdown para pilotos, equipas, circuitos, etc.-->
+                <li class="nav-item">
+                    <a class="nav-link" href="categorias.php">Categorias</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="wec-index.php">WEC</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="showroom.php">Showroom</a>
+                    <a class="nav-link" href="cargos.php">Cargos</a>
                 </li>
             </ul>
         </div>
