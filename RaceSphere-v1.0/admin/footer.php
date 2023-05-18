72752ea7de456d89e9c20910d350de02c2b76097
Paulo André Leal <pauloandeleal@gmail.com>
2023-05-18 15:33:09 +0100

    asdasd

---
+++ RaceSphere-v1.0/admin/footer.php
@@ -0,0 +1,19 @@
+<div class="container-fluid alert w-50 align-items-center alert-success fixed-bottom alert-dismissible fade text-center" role="alert" id="alertBox">
+  Alerta, tudo foi feito
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
+<div class="container">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="sobre.php" class="nav-link px-2 text-muted">Sobre</a></li>
+      <li class="nav-item"><a href="tercod.php" class="nav-link px-2 text-muted">Termos e Condições</a></li>
+      <li class="nav-item"><a href="privacyPolicy.php" class="nav-link px-2 text-muted">Política e Privacidade</a></li>
+      <li class="nav-item"><a href="FAQ.php" class="nav-link px-2 text-muted">FAQ's</a></li>
+      <li class="nav-item"><a href="contactos.php" class="nav-link px-2 text-muted">Contactos</a></li>
+    </ul>
+    <p class="text-center text-muted">© 2023 RaceSphere</p>
+  </footer>
+</div>
---
+++ RaceSphere-v1.0/admin/navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary sticky-md-top">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-brand"><img src="img/logo.svg" alt="RaceSphere" height="60" width="60"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand " href="index.php"><img src="img/logoExt.svg" alt="RaceSphere" height="30"
+                    class="d-inline-block align-top"></a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="f1-index.php">Utilizadores</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="rally-index.php">Noticias</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="wec-index.php">WEC</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="showroom.php">Showroom</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
