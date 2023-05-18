4158458a1f2f1e0ac4c4db8b88f1eadee28065b4
Paulo André Leal <pauloandeleal@gmail.com>
2023-05-18 15:27:47 +0100

    nkjajhsjdasd

---
+++ RaceSphere-v1.0/admin/news.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include('bootstrapInc.php'); ?>
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/pauloLeal.css">
+</head>
+<body>
+    
+</body>
+</html>
