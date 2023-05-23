91d1bcf81a0ceb6b50633d92ab5dfacc2a678ebf
Licosapo <128393331+Licosapo@users.noreply.github.com>
2023-05-23 16:53:15 +0100

    edit de users feito :)

--- RaceSphere-v1.0/admin/editUsers.php
+++ RaceSphere-v1.0/admin/editUsers.php
@@ -18,15 +18,36 @@
     include 'navbar.php';
     include '../sqli/conn.php';
     $editar = $_GET["id"];
+    $query = "select email_admin, cargo_admin, nome_admin from administrador where id_admin='" . $editar . "'";
+    $result_set = $conn->query($query);
+    if ($result_set) {
+        while ($row = $result_set->fetch_assoc()) {
+            $cargo = $row['cargo_admin'];
+            $nome = $row['nome_admin'];
+            $email = $row['email_admin'];
+            $pass = $row['cargo_admin'];
+        }
+    }
     ?>
-    <form action="editUsers.php?id=<?=$editar?>" method="POST">
+    <form action="editUsers.php?id=<?= $editar ?>" method="POST">
         <center><br>
-            <input type="text" name="email" placeholder="Email"><br><br>
-            <input type="text" name="nome" placeholder="Nome"><br><br>
-            <select name="cargo">
-                <option value="admin">Administrador</option>
-                <option value="pressman">Pressman</option>
-            </select><br><br>
+            <input type="text" name="nome" placeholder=<?php echo "'".$nome."'"?>><br><br>
+            <?php
+            if ($cargo == "admin") { ?>
+                <select name="cargo">
+                    <option value="admin" selected>Administrador</option>
+                    <option value="pressman">Pressman</option>
+                </select><br><br>
+                <?php
+            } else {
+                ?>
+                <select name="cargo">
+                    <option value="admin">Administrador</option>
+                    <option value="pressman" selected>Pressman</option>
+                </select><br><br>
+                <?php
+            }
+            ?>
             <input type="submit" name="editar2">
         </center>
     </form>
@@ -35,40 +56,26 @@
         $emailnovo = $_POST["email"];
         $nomenovo = $_POST["nome"];
         $cargonovo = $_POST["cargo"];
-        $query = "select email_admin, cargo_admin, nome_admin from administrador where id_admin='" . $editar . "'";
-        $result_set = $conn->query($query);
-        if ($result_set) {
-            while ($row = $result_set->fetch_assoc()) {
-
-                $nome = $row['nome_admin'];
-                $email = $row['email_admin'];
-                $pass = $row['cargo_admin'];
-            }
-            if ($emailnovo == "") {
-                $emailnovo = $email;
-            }
-            if ($nomenovo == "") {
-                $nomenovo = $nome;
-            }
-            $edit = "UPDATE administrador SET email_admin = '" . $emailnovo . "', nome_admin = '" . $nomenovo . "' , cargo_admin = '" . $cargonovo . "' WHERE administrador.id_admin ='" . $editar . "'";
-            $result_set = $conn->query($edit);
-            if ($result_set) {
-                ?>
-                <script>
-                    window.setTimeout(function () {
-                        location.href = "userManagement.php";
-                    }, 0);
-                </script>
-                <?php
-            } else {
-                echo "Query update mal feita";
-            }
-        } else {
-            echo "Query select feita";
+        if ($emailnovo == "") {
+            $emailnovo = $email;
         }
-
+        if ($nomenovo == "") {
+            $nomenovo = $nome;
+        }
+        $edit = "UPDATE administrador SET nome_admin = '" . $nomenovo . "' , cargo_admin = '" . $cargonovo . "' WHERE administrador.id_admin ='" . $editar . "'";
+        $result_set = $conn->query($edit);
+        if ($result_set) {
+            ?>
+            <script>
+                window.setTimeout(function () {
+                    location.href = "userManagement.php";
+                }, 0);
+            </script>
+            <?php
+        } else {
+            echo "Query update mal feita";
+        }
     }
-
     include '../footer.php';
     ?>
 </body>
