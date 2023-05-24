cbb0c9bb11d4186ec80f69058251ee278f1f8d2f
AlexCosta-proj <alex.rodrcosta@gmail.com>
2023-05-24 23:47:28 +0100

    n

--- Daniel Ribeiro/Ficha5/login.php
+++ Daniel Ribeiro/Ficha5/login.php
@@ -23,6 +23,7 @@ session_start();
         $user = "root";
         $pass = "";
         $conn = new mysqli($host, $user, $pass, $database);
+        
         if ($conn->connect_errno) {
             echo "Failed to connect to MySQL: " . $conn->connect_error;
             exit();
--- RaceSphere-v1.0/login.php
+++ RaceSphere-v1.0/login.php
@@ -12,49 +12,45 @@
 
 </head>
 
-
 <body>
-
-    <section class="vh-100" style="background-color: #9A616D;">
-        <div class="container py-5 h-100">
+    <form action = "login.php" method="post">
+        <div class="container py-5 h-100 vh-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col col-xl-12">
                     <div class="card" style="border-radius: 1rem;">
                         <div class="row g-0">
-                            <div class="col-md-6 col-lg-5 d-none d-md-block">
-                                <img src="img/img_alex/loginimg.jpg" alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                            <div class="col-md-5 col-lg-5 d-none d-md-block">
+                                <img src="img/img_alex/loginIMG.jpg" alt="login form"
+                                        class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
                             </div>
+
                             <div class="col-md-6 col-lg-7 d-flex align-items-center">
                                 <div class="card-body p-7 p-lg-6 text-black">
 
                                     <form>
 
-                                        <div class="d-flex align-items-center mb-3 pb-1">
-                                        <span class="navbar-brand"><img src="img/logo.svg" alt="RaceSphere" height="50" width="50"></span>
-                                            <span class="h1 fw-bold mb-0">RaceSphere</span>
-                                        </div>
-
-                                        <h5 class="fw-normal mb-1 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
-    
-                                        <div class="form-outline mb-3">
-                                            <input type="email" id="form2Example17"
-                                                class="form-control form-control-lg" />
-                                            <label class="form-label" for="form2Example17">Email address</label>
+                                        <div class="d-flex align-items-center">
+                                           <a href="index.php"> <span class="navbar-brand"><img src="img/logoExt.svg" alt="RaceSphere" height="90" width="220"></span></a>
                                         </div>
 
                                         <div class="form-outline mb-3">
-                                            <input type="password" id="form2Example27"
-                                                class="form-control form-control-lg" />
-                                            <label class="form-label" for="form2Example27">Password</label>
+                                            <label class="form-label" for="email">Email</label>
+                                            <input type="email" id="email" class="form-control form-control-lg" name="email"/>
+                                            
                                         </div>
 
+                                        <div class="form-outline mb-3">
+                                            <label class="form-label" for="form2Example27">Palavra Passe</label>
+                                            <input type="password" id="form2Example27" class="form-control form-control-lg" name="pass" />
+                                            
+                                        </div>
+
+                                        
                                         <div class="pt-1 mb-4">
-                                            <button class="btn btn-dark btn-lg btn-block" type="button">Login</button>
+                                            <button class="btn btn-dark btn-lg btn-block" type="button">Iniciar Sessão</button>
                                         </div>
 
-                                        <a class="small text-muted" href="#!">Forgot password?</a>
-                                        <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a
-                                                href="#!" style="color: #393f81;">Register here</a></p>
+                                        <p>Ainda não tem conta? <a href="register.php" style="color: #393f81;">Registe-se aqui</a></p>
                                     </form>
 
                                 </div>
@@ -64,14 +60,7 @@
                 </div>
             </div>
         </div>
-    </section>
-
-
-
-
-
-
-
+    </form>
 
     <?php
     if (isset($_POST['submit'])) {
@@ -87,15 +76,16 @@
             $email = $_POST['email'];
             $plaintext_password = $_POST['pass'];
             $password = hash('sha512', $plaintext_password);
-            $query = "select email, pass, nome from utilizador where email='" . $email . "' and pass='" . $password . "'";
+            $query = "select email, pass from utilizador where email='" . $email . "' and pass='" . $password . "'";
             $result_set = $conn->query($query);
+
             if ($result_set) {
                 if ($result_set->num_rows == 1) {
                     while ($row = $result_set->fetch_assoc()) {
 
                         $nome = $row['nome'];
                         $_SESSION = array();
-                        $_SESSION["email"] = $email;
+                        $_SESSION["email"] = $email_user;
                         $_SESSION["nome"] = $nome;
                     }
                     if ($email == "admin@gmail.com") { ?>
