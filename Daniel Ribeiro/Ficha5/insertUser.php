a63fb664e7bc3df66dd15325186666b05f833790
Licosapo <128393331+Licosapo@users.noreply.github.com>
2023-05-30 11:58:04 +0100

    ajax a funcionar no listuser.php + getuser.php

--- Daniel Ribeiro/Ficha5/insertUser.php
+++ Daniel Ribeiro/Ficha5/insertUser.php
@@ -9,11 +9,11 @@ session_start();
 </head>
 
 <body>
-    <form action="login.php" method="POST">
+    <form action="insertUser.php" method="POST" enctype="multipart/form-data">
         <center><br>
             <input type="text" name="email" placeholder="Email"><br><br>
             <input type="text" name="nome" placeholder="nome"><br><br>
-            <input type="file" name="pass"><br><br>
+            <input type="file" name="foto"accept="image/*"><br><br>
             <input type="text" name="pass" placeholder="Password"><br><br>
             <input type="submit" name="submit">
         </center>
@@ -34,36 +34,29 @@ session_start();
             echo "Failed to connect to MySQL: " . $conn->connect_error;
             exit();
         } else {
-            $email = $_POST['email'];
-            $plaintext_password = $_POST['pass'];
-            $password = hash('sha512', $plaintext_password);
-            $query = "select email, pass, nome from utilizador where email='" . $email . "' and pass='" . $password . "'";
-            $result_set = $conn->query($query);
-            if ($result_set) {
-                if ($result_set->num_rows == 1) {
-                    while ($row = $result_set->fetch_assoc()) {
-                        
-                        $nome = $row['nome'];
-                        $_SESSION = array();
-                        $_SESSION["email"] = $email;
-                        $_SESSION["nome"] = $nome;
+            if (isset($_FILES['foto']) && $_FILES['foto']['error'] === UPLOAD_ERR_OK) {
+                $file = $_FILES['foto'];
+                // Especifique o caminho para a pasta onde deseja guardadar as imagens
+                $uploadDirectory = 'upload_img/';
+                // Gera um nome único para o arquivo
+                $fileName = uniqid() . '_' . $file['name'];
+                $destination = $uploadDirectory . $fileName;
+                // Verifica se o tipo de arquivo é uma imagem
+                if (exif_imagetype($file['tmp_name'])) {
+                    // Move o arquivo para a pasta de destino
+                    if (move_uploaded_file($file['tmp_name'], $destination)) {
+    
+                        $query = "INSERT INTO utilizador (email, nome, pass, foto) VALUES ('$email','$nome','$password','$fileName');";
+                        $conn->query($query);
+                        //prompt msg a dizer q a imagem foi guardada
+                    } else {
+                        echo 'Ocorreu um erro ao guardar a imagem.';
                     }
-                    if ($email == "admin@gmail.com") { ?>
-                        <script>
-                            window.setTimeout(function () {
-                                location.href = "listUser.php";
-                            }, 3000);
-                        </script>
-                        <?php
-                    }
-                    echo "Bem-Vindo ".$_SESSION["nome"];
                 } else {
-                    echo "Erro na autenticação credenciais erradas tente de novo";
+                    echo 'O ficheiro enviado não é uma imagem válida.';
                 }
             } else {
-                $code = $conn->errno; // error code of the most recent operation
-                $message = $conn->error; // error message of the most recent op.
-                printf("<p>Query error: %d %s</p>", $code, $message);
+                echo 'Nenhum ficheiro foi enviado.';
             }
         }
     }
