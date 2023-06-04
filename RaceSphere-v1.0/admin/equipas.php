baa9b8d45c874161d53c51c5d0aa94a1f884128c
AlexCosta-proj <alex.rodrcosta@gmail.com>
2023-06-04 15:20:12 +0100

    a

--- RaceSphere-v1.0/admin/equipas.php
+++ RaceSphere-v1.0/admin/equipas.php
@@ -1,6 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <link rel="stylesheet" href="richtexteditor/rte_theme_default.css" />
+    <script type="text/javascript" src="richtexteditor/rte.js"></script>
+    <script type="text/javascript" src='richtexteditor/plugins/all_plugins.js'></script>
+    <?php include('bootstrapInc.php'); ?>
+    <title>Gerir Equipas</title>
+    <link rel="stylesheet" href="../css/alex.css">
+
+</head>
+
+<body>
+    <?php
+    include('navbar.php');
+    ?>
+
+    <table class="table" style="height: auto; width: 50%; align-items: center;margin-left: auto; margin-right: auto;">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome Equipa</th>
+                <th scope="col">Categoria Equipa</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+            </tr>
+
+        </tbody>
+    </table>
 
 
 
+    <?php include('footer.php'); ?>
+</body>
 
-
+</html>
 <!-- LOGO DA EQUIPA TENHO NA CENA DO PATOLAS COM A IMG DAS Noticias-->
