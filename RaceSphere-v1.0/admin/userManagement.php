0f51a3b60dd5db64733323298d386ab66f3deb55
Licosapo <128393331+Licosapo@users.noreply.github.com>
2023-06-05 16:41:55 +0100

    a

--- RaceSphere-v1.0/admin/userManagement.php
+++ RaceSphere-v1.0/admin/userManagement.php
@@ -23,52 +23,67 @@
         <a href="addUsers.php"><button class="btn btn-primary btn-lg">Adicionar Users</button></a>
     </div>
     <div id="tabelasListagemUsers">
+        
         <?php
-        $query = "select * from administrador order by nome_admin";
+        //Tabela Users
+        $query = "select * from utilizador where cargo_user!='' order by cargo_user, nome_user";
         $result_set = $conn->query($query);
         if ($result_set) {
             ?>
-            <h1>Admins e Pressmans</h1>
+            <h1>Admin/Pressman</h1>
             <table class="table table-success table-striped-columns">
                 <tr>
                     <th>Id</th>
                     <th>Nome</th>
-                    <th>Cargo</th>
                     <th>Email</th>
+                    <th>Telefone</th>
+                    <th>Cargo</th>
                     <th>Editar contas</th>
                     <th>Eliminar contas</th>
                 </tr>
                 <?php
                 while ($row = $result_set->fetch_assoc()) {
-                    $id_user = $row['id_admin'];
+                    $id_user = $row['id_user'];
                     ?>
                     <tr>
                         <td>
                             <?php
-                            echo $row['id_admin'];
+                            echo $row['id_user'];
                             ?>
                         </td>
                         <td>
                             <?php
-                            echo $row['nome_admin'];
+                            echo $row['nome_user'];
                             ?>
                         </td>
                         <td>
                             <?php
-                            echo $row['cargo_admin'];
+                            echo $row['email_user'];
                             ?>
                         </td>
                         <td>
                             <?php
-                            echo $row['email_admin'];
+                            echo $row['telefone_user'];
                             ?>
                         </td>
                         <td>
-                            <form action="editAdminPress.php" method="POST"><button><a
+                            <?php
+                                if($row['cargo_user']=="press"){
+                                    $row['cargo_user'].="man";
+                                }else{
+                                    $row['cargo_user'].="istrador";
+                                }
+                                $cargo_user=ucfirst($row['cargo_user']);
+                                echo $cargo_user;
+                            
+                            ?>
+                        </td>
+                        <td>
+                            <form action="editUsers.php" method="POST"><button><a
                                         href="editAdminPress.php?id=<?= $id_user ?>">Editar</a></button></form>
                         </td>
                         <td>
-                            <form action="userManagement.php" method="POST"><button name="eliminar" type="submit"
+                            <form action="userManagement.php" method="POST"><button name="eliminaruser" type="submit"
                                     value="<?php echo $id_user ?>" onclick='this.form.submit()'>Eliminar</button></form>
                         </td>
                     </tr>
@@ -84,11 +99,11 @@
             printf("<p>Query error: %d %s</p>", $code, $message);
         }
         //Tabela Users
-        $query = "select * from utilizador order by nome_user";
+        $query = "select * from utilizador where cargo_user IS NULL order by nome_user";
         $result_set = $conn->query($query);
         if ($result_set) {
             ?>
-            <h1>Users</h1>
+            <h1>Utilizadores</h1>
             <table class="table table-success table-striped-columns">
                 <tr>
                     <th>Id</th>
@@ -144,20 +159,6 @@
             printf("<p>Query error: %d %s</p>", $code, $message);
         }
 
-        if (isset($_POST['eliminar'])) {
-            $eliminar = $_POST['eliminar'];
-            $delete = "DELETE FROM administrador where id_admin='$eliminar'";
-            $result_set = $conn->query($delete);
-            if ($result_set) {
-                ?>
-                <script>
-                    window.setTimeout(function () {
-                        location.href = "userManagement.php";
-                    }, 0);
-                </script>
-                <?php
-            }
-        }
         if (isset($_POST['eliminaruser'])) {
             $eliminar = $_POST['eliminaruser'];
             $delete = "DELETE FROM utilizador where id_user='$eliminar'";
