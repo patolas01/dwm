086ce6f8022246fbc37d1ce988b3b833342db39c
Paulo André Leal <pauloandeleal@gmail.com>
2023-06-07 11:01:03 +0100

    kjdhhasd

--- RaceSphere-v1.0/navbar.php
+++ RaceSphere-v1.0/navbar.php
@@ -28,6 +28,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="equipas.php">Equipas</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="piloto.php">Pilotos</a>
+                </li>
             </ul>
 
 
