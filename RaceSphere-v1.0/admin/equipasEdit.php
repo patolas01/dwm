09e6795dc124164ed979b96ab027bfd537639ac9
AlexCosta-proj <alex.rodrcosta@gmail.com>
2023-06-16 19:52:13 +0100

    a

--- RaceSphere-v1.0/admin/equipasEdit.php
+++ RaceSphere-v1.0/admin/equipasEdit.php
@@ -84,7 +84,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Criar Equipas</title>
+    <title>Editar Equipas</title>
     <?php include('bootstrapInc.php'); ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
