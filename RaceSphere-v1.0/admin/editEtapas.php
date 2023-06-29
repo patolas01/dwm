084bb37ba20e3610d16e36563df752bf8cf4b373
Licosapo <128393331+Licosapo@users.noreply.github.com>
2023-06-29 18:12:09 +0100

    nao consigo por merda nenhuma a funcionar fodasse

--- RaceSphere-v1.0/admin/editEtapas.php
+++ RaceSphere-v1.0/admin/editEtapas.php
@@ -61,20 +61,20 @@
                                 </div>
                                 <div class="col-md-12">
                                     Dia Etapa:
-                                    <input class="form-control" id="diaEtapa" onfocus="(this.type='date')" type="text" name="dia"
-                                        value=<?php echo "'" . $dia . "'" ?>>
+                                    <input class="form-control" id="diaEtapa" onfocus="(this.type='date')" type="text"
+                                        name="dia" value=<?php echo "'" . $dia . "'" ?>>
                                 </div>
                                 <h5 id="diaEtapaCheck"></h5>
                                 <div class="col-md-12">
                                     Inicio Etapa:
-                                    <input class="form-control" onfocus="(this.type='time')" type="text" name="inicio" id="horaInicio"
-                                        value=<?php echo "'" . $inicio . "'" ?>>
+                                    <input class="form-control" onfocus="(this.type='time')" type="text" name="inicio"
+                                        id="horaInicio" value=<?php echo "'" . $inicio . "'" ?>>
                                 </div>
                                 <h5 id="horaInicioCheck"></h5>
                                 <div class="col-md-12">
                                     Fim Etapa:
-                                    <input class="form-control" onfocus="(this.type='time')" type="text" name="fim" id="horaFim"
-                                        value=<?php echo "'" . $fim . "'" ?>>
+                                    <input class="form-control" onfocus="(this.type='time')" type="text" name="fim"
+                                        id="horaFim" value=<?php echo "'" . $fim . "'" ?>>
                                 </div>
                                 <h5 id="horaFimCheck"></h5>
                                 <br>
@@ -110,36 +110,48 @@
         // buscar datas das rondas anterior e seguinte
         $antes = $rondanovo - 1;
         $depois = $rondanovo + 1;
-        $query1 = "SELECT * FROM etapa where num_etapa='$antes' and id_prova='$editar'";
+        $query1 = "SELECT etapa.*,inicio_prova FROM etapa left join prova on etapa.id_prova=prova.id_prova where num_etapa='$antes' and etapa.id_prova='$id_prova'";
         $result_set1 = $conn->query($query1);
         if ($result_set1) {
-            while ($row1 = $result_set1->fetch_assoc()) {
-                $dataAnterior = $row1['dia_etapa'];
-                
+            if ($result_set1->num_rows == 0) {
+                $dataAnterior = $row1['inicio_prova'];
+            } else {
+                while ($row1 = $result_set1->fetch_assoc()) {
+                    $dataAnterior = $row1['dia_etapa'];
+
+                }
             }
         }
-        $query2 = "SELECT * FROM etapa where num_etapa='$depois' and id_prova='$editar'";
+        $query2 = "SELECT etapa.*,prova.fim_prova FROM etapa left join prova on etapa.id_prova=prova.id_prova where num_etapa='$depois' and etapa.id_prova='$id_prova'";
         $result_set2 = $conn->query($query2);
         if ($result_set2) {
-            while ($row2 = $result_set2->fetch_assoc()) {
-                $dataSeguinte = $row2['dia_etapa'];
+            if ($result_set2->num_rows == 0) {
+                while ($row2 = $result_set2->fetch_assoc()) {
+                    $dataSeguinte = $row2['fim_prova'];
+                }
+            } else {
+                while ($row2 = $result_set2->fetch_assoc()) {
+                    $dataSeguinte = $row2['dia_etapa'];
+                }
             }
         }
-        $query = "SELECT * FROM etapa where num_etapa='$rondanovo' and id_prova='$editar'";
+        $query = "SELECT * FROM etapa where num_etapa='$rondanovo' and id_prova='$id_prova'";
         $result_set = $conn->query($query);
         if ($result_set) {
             //$jaexiste = mysqli_num_rows($result_set); tentar isto
-            if ($result_set->num_rows == 1) {
+            if ($result_set->num_rows == 1 && $rondanovo != $numero) {
                 ?>
                 <script>alert("Esta ronda já existe");</script>
                 <?php
-            } elseif ($dia < $dataAnterior || $dia > $dataSeguinte) {
+            } elseif ($dianovo < $dataAnterior || $dianovo > $dataSeguinte) {
                 ?>
-                <script>alert("Data errada <?php echo $dia ?>");</script>
+                <script>alert("Data deve estar entre <?php echo $dataAnterior ?> e <?php echo $dataSeguinte ?>");</script>
                 <?php
 
             } else {
-
+                ?>
+                <script>alert("Data minima-><?php echo $dataAnterior ?> Data manima-> <?php echo $dataSeguinte ?>Data inserida-> <?php echo $dataSeguinte ?>");</script>
+                <?php
                 $edit = "UPDATE etapa SET num_etapa='$rondanovo' , dia_etapa = '" . $dianovo . "' , inicio_etapa = '" . $inicionovo . "' , fim_etapa = '" . $fimnovo . "' WHERE etapa.id_etapa ='" . $editar . "'";
                 $result_set = $conn->query($edit);
                 if ($result_set) {
