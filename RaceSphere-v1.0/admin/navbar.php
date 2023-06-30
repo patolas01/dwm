35790a49d70d113e24a36b265cba651fa11055b7
Silvafire <128393372+Silvafire@users.noreply.github.com>
2023-06-30 20:58:02 +0100

    a

--- RaceSphere-v1.0/admin/navbar.php
+++ RaceSphere-v1.0/admin/navbar.php
@@ -8,9 +8,6 @@
             <a class="navbar-brand " href="../index.php"><img src="../img/logoExt.svg" alt="RaceSphere" height="30"
                     class="d-inline-block align-top"></a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-                    <a class="nav-link" href="index.php">Index</a>
-                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="userManagement.php">Utilizadores</a>
                 </li>
